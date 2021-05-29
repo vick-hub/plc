4843f1b8f819d26cd0379f86eff6fcee6828323b
paulkorir <paul@sci2prol.com>
2021-05-29 09:56:55 +0100

    show operators

--- week2/ops.py
+++ week2/ops.py
@@ -4,8 +4,21 @@ import os
 
 def main():
     # operators: +, -, *, /, parentheses
+    x = 1
+    y = 2
+    print(x + y)
+    print(x * y)
+    print(x / y)
+    print(x - y)
+    # BODMAS - brackets of division multiplication addition subtraction
+    z = 7
+    print(x + y * z)
+    print((x + y) * z)
+    print(2 // 7) # integer division
+    print(2 % 7) # modulo operator
     # numeric
     # str
+    print('lskdjfldj' + '1!!!!!!')
     return os.EX_OK
 
 

239ae75ffaf87834a5c5c6de0867b2b0bec29744
paulkorir <paul@sci2pro.com>
2021-06-12 10:34:12 +0100

    updated notes

--- week4/dict.py
+++ week4/dict.py
@@ -1,34 +1,112 @@
 import os
 import sys
+import random
+import string
 
 
 def main():
     # 1. creating dictionaries
-
-    # 2. retrieving items from dictionaries
-    # d[key]
-    # d.get(key, [default])
-    # KeyError
-
+    d = dict()  # class
+    print(d, type(d))
+    d2 = {"triangle": 'a', "circle": 'b'}  # literal
+    print(d2, type(d2))
+    d3 = dict(triangle='a', circle='b')  # kwargs
+    print(d3, type(d3))
+    d4 = dict({"triangle": 'a', "circle": 'b'}, square='d', rectangle='e')  # mix and match
+    print(d4, type(d4))
+    keys = random.choices(range(10), k=10)
+    values = random.choices(range(10), k=10)
+    print(f"keys = {keys}")
+    print(f"values = {values}")
+    print(list(zip(keys, values)))  # zip
+    d5 = dict([(8, 2), (0, 9), (1, 6), (3, 1), (2, 1), (2, 2), (6, 9), (7, 3), (2, 0), (2, 2)])
+    print(d5)
+    d6 = dict(zip(keys, values))
+    print(d6)
+    d7 = dict(zip(string.ascii_lowercase, values))
+    print(d7)
+    #
+    # # 2. retrieving items from dictionaries
+    # print(d7['a'])
+    # print(d[3])
+    print(d6)
+    try:
+        print(f"value with key = 3 is {d6[3]}")
+    except KeyError:
+        print(f"default value is 999")
+    # # d.get(key, [default])
+    print(f"default value is {d6.get(3, 999)}")
+    # # d[key]
+    # # KeyError
+    #
     # 3. modifying dict content
     # d[key] = value
+    shapes = {
+        'triangle': 234,
+        'circle': 9893,
+        'square': 3782,
+        'rectangle': 993,
+        'rhombus': 232,
+        'trapezium': 93,
+    }
+    print(shapes)
+    shapes['circle'] = 555
+    print(shapes)
     # d.setdefault(key, [default])
+    shapes['oval'] = 777
+    print(shapes)
+    # print(shapes['diamond'])
+    print("diamond has ", shapes.setdefault('diamond', 464))
+    print(shapes)
+    # crud: create, read, update and delete
     # del d[key]
+    del shapes['square']
+    print(shapes)
 
     # 4. membership (key in, key not in)
+    print("Is square in shapes?", 'square' in shapes)  # False
+    print("Is oval in shapes?", 'oval' in shapes)  # True
+    print("Is square not in shapes?", 'square' not in shapes)  # True
 
-    # 5. castings on dict
+    # 5. castings/function on dict
     # len(d)
-    # iter(d)
+    print(len(shapes))
+    # iter(d) -> iterator over keys of dict
+    print(iter(shapes))
+    for k in iter(shapes):
+        print(f"key = {k}")
     # list(d)
+    print(shapes)
+    print(list(shapes))  # only keys
 
     # 6. dict methods
     # dict.get(key, [default])
     # dict.setdefault(key, [default])
-    # dict.items()
+    # # dict.items()
+    # for x in shapes:
+    #     print(x, shapes[x])
+    print(shapes.items())
+    for key, value in shapes.items():
+        print(f"The shape {key} has areas {value}")
     # dict.keys()
+    print(f"Keys: {shapes.keys()}")
     # dict.values()
-    # dict.update()
+    print(f"Value: {shapes.values()}")
+    # # lists as values
+    fancy = dict()
+    fancy[(0, 0)] = random.choices(range(10), k=3)
+    fancy[(0, 1)] = random.choices(range(10), k=3)
+    fancy[(1, 0)] = random.choices(range(10), k=3)
+    fancy[(1, 1)] = random.choices(range(10), k=3)
+    print(fancy)
+    # # dict.update()
+    fancy2 = dict()
+    fancy2[(1, 1)] = random.choices(range(10), k=3)
+    fancy2[(1, 2)] = random.choices(range(10), k=3)
+    fancy2[(2, 2)] = random.choices(range(10), k=3)
+    print(fancy2)
+    fancy.update(fancy2)
+    print(fancy)
     return os.EX_OK
 
 
--- week4/set.py
+++ week4/set.py
@@ -1,29 +1,83 @@
 import os
 import sys
+import random
 
 
 def main():
     # 1. creating sets
     # set()
+    s = set()
+    print(s, type(s))
+    s1 = {}
+    print(s1, type(s))
+    s2 = {1, 2, 3}
+    print(s2)
+    s3 = set([1, 2, 3, 4])
+    print(f"s3 = {s3}")
+    # print(s3[0])
     # frozenset()
+    f1 = frozenset()
+    print(f1, type(f1))
 
     # 2. modifying sets
     # set.add(elem)
+    s3.add(27)
+    print(s3)
+    for i in random.choices(range(10, 20), k=10):
+        s3.add(i)
+        print(i)
+    print(s3)
     # set.remove(elem)
+    s3.remove(27)
+    print(s3)
+    # s3.remove(99)  # KeyError
     # set.discard(elem)
+    s3.discard(99)  # Avoid KeyError
 
     # 3. membership (in, not in)
 
     # 4. castings on sets
     # len(s)
+    list_s3 = list(s3)
+    print(list_s3, type(list_s3))
     # list(s), tuple(s)
 
     # 5. methods/operations
+    s1 = set(random.choices(range(100, 115), k=15))
+    s2 = set(random.choices(list(s1), k=10))
+    print(f"s1 = {s1}, {len(s1)}")
+    print(f"s2 = {s2}, {len(s2)}")
     # <=, <, set.issubset(other)
+    print(f"s2 is a subset of s1: {s2 <= s1}")  # subset
+    print(f"s2 is a proper subset of s1: {s2 < s1}")  # proper subset
     # >=, >, set.issuperset(other)
+    print(f"s1 is a proper superset of s2: {s1 > s2}")
+    s5 = set(random.choices(range(100, 115), k=15))
+    s6 = set(random.choices(range(100, 115), k=15))
+    print(f"s5 = {s5}, {len(s5)}")
+    print(f"s6 = {s6}, {len(s6)}")
     # |, set.union(*others)
+    s7 = s5.union(s6) # combined
+    print(f"union = {s7}, {len(s7)}")
+    print(s5 | s6)
     # &, set.intersection(*others)
+    s8 = s5.intersection(s6)
+    print(f"intersect = {s8}, {len(s8)}") # occurs in both
     # -, set.difference(*others)
+    s9 = s5.difference(s6)
+    s10 = s6.difference(s5)
+    print(s5)
+    print(s6)
+    print(f"difference = {s9}, {len(s9)}")
+    print(f"difference = {s10}, {len(s10)}")
+
+    f = frozenset([1, 2, 3, 4, 5])
+    f1 = frozenset(range(10, 15))
+    # f.add(10)
+    print(f"frozenset = {f}")
+    print(f"frozenset = {f1}")
+    f2 = f | f1
+    print(f"union: {f2}")
     # ^, set.symmetric_difference(other)
     # set.copy()
     # set.pop()
@@ -33,6 +87,9 @@ def main():
     # &=, set.intersection_update(*others)
     # -=, set.difference_update(*others)
     # ^=, set.symmetric_difference_update(other)
+
+    # test your grasp on the fundamentals: https://bit.ly/3xlpwnz
+    # feedback: https://bit.ly/3pLm9U9
     return os.EX_OK
 
 

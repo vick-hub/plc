8652541aae34c302057a9a59f6df4bf00b5a142b
paulkorir <paul@sci2pro.com>
2021-07-02 22:19:00 +0100

    week 6 stubs

---
+++ week6/functions.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+
+def main():
+    # example 1
+    # x1, x2 = calculate(1, 2, 3)
+
+    # example 2
+    # add(1)
+    # add(1, 3)
+    # add(1, extra=3)
+
+    # global vs. local scope
+
+    # positional parameters
+    # default parameters
+
+    # anonymous functions
+
+    # map()
+
+    # filter()
+
+    return os.EX_OK
+
+
+if __name__ == "__main__":
+    sys.exit(main())

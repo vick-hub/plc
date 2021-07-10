430a240e617f975d9948458287cecd6f5fe27fbc
paulkorir <paul@sci2pro.com>
2021-07-10 06:38:26 +0100

    notes outlines

--- week7/files.py
+++ week7/files.py
@@ -3,6 +3,40 @@ import sys
 
 
 def main():
+    # official dox: https://docs.python.org/3/tutorial/inputoutput.html#tut-files
+    # vanilla file handling
+    # f = open(<filename>, <mode>, ...)
+    # print(f)
+    # print(type(f))
+    # f.close()
+
+    # file opening modes
+    # 'r', 'w', 'a', 'b', 't'
+    # war_and_peace.txt
+    # image.jpg
+    # example of appending to a file
+
+    # with context manager
+    # to cleanly handle closing of files
+
+    # reading data
+    # f.read() -> everything
+    # f.readline() -> str
+    # f.readlines() -> list
+
+    # writing to a file
+    # f.write()
+    # print("something", file=<file_object>)
+
+    # navigating through a file
+    # f.tell()
+    # f.seek()
+
+    # exceptions
+    # FileExistsError
+    # FileNotFoundError
+    # see the notes for more errors
+
     return os.EX_OK
 
 
--- week7/paths.py
+++ week7/paths.py
@@ -3,6 +3,19 @@ import sys
 
 
 def main():
+    # official documentation: https://docs.python.org/3/library/pathlib.html
+    # import the pathlib library
+    # creating Path objects
+
+    # using the / operator: https://docs.python.org/3/library/pathlib.html#operators
+
+    # methods: https://docs.python.org/3/library/pathlib.html#operators
+    # Path.name
+    # Path.parent
+    # Path.exists()
+    # Path.is_dir()
+
+    # Path.mkdir() analogous to mkdir in Linux
     return os.EX_OK
 
 

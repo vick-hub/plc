cb8f087f54dd8eb36cba1796e5007d309827cbd1
vick-hub <amondev00@gmail.com>
2021-07-21 17:06:27 +0300

    modify problem4.py

--- week7/problem4.py
+++ week7/problem4.py
@@ -5,10 +5,13 @@ import pathlib
 
 def main():
     new_dir = pathlib.Path("mars/mist")
-    new_dir.mkdir()
-    print(new_dir.exists())
-    with open('reduce-nut.txt', 'r+') as f:
-        print(f.read())
+    try:
+        new_dir.mkdir()
+    except OSError as error:
+        print(error)
+    new_file = pathlib.Path(new_dir, 'reduce-nut.txt')
+    with open(new_file, 'w') as f:
+        file = f.write('Hello, practicing working with files and path')
     return os.EX_OK
 
 

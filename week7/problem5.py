9e0806bbd421e594fb89a04be837507c89f68c50
vick-hub <amondev00@gmail.com>
2021-07-22 12:04:24 +0300

    modify problem5.py

--- week7/problem5.py
+++ week7/problem5.py
@@ -6,13 +6,17 @@ import pathlib
 def main():
     path = pathlib.Path('cortex')
     print(path)
+    try:
+        os.mkdir(path)
+    except OSError:
+        pass
     path1 = path/'nothing.txt'
     print(path1)
-    with open('nothing.txt', 'r') as f:
+    with open(path/'nothing.txt', 'r') as f:
         f.read()
     path2 = path/'image.jpg'
     print(path2)
-    with open('image.jpg', 'rb') as f:
+    with open(path/'image.jpg', 'rb') as f:
         f.read()
     path3 = path/'orange.json'
     print(path3)

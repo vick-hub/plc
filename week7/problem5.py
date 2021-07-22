485d32fa10bbaf2f90e92e5fc1040dd48a58c7f0
vick-hub <amondev00@gmail.com>
2021-07-22 11:55:02 +0300

    modify problem5

--- week7/problem5.py
+++ week7/problem5.py
@@ -8,8 +8,12 @@ def main():
     print(path)
     path1 = path/'nothing.txt'
     print(path1)
+    with open('nothing.txt', 'r') as f:
+        f.read()
     path2 = path/'image.jpg'
     print(path2)
+    with open('image.jpg', 'rb') as f:
+        f.read()
     path3 = path/'orange.json'
     print(path3)
     path4 = '/home/victor/path'

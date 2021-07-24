63f2f282fd51192a21fc742f71a6b3c2d366255c
vick-hub <amondev00@gmail.com>
2021-07-24 14:45:38 +0300

    modify problem0.py

--- week7/problem0.py
+++ week7/problem0.py
@@ -7,7 +7,7 @@ def main():
         with open('new_file.txt', 'w+', encoding='utf-8') as ft:
             for line in fr:
                 ft.write(line[:-1] + '\r\n')  # select all but leaves the last newline
-
+                ft.read()
     return os.EX_OK
 
 

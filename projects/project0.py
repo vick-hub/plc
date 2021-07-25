131508c2824de7ada4193a72db9ee511cc122edd
vick-hub <amondev00@gmail.com>
2021-07-25 14:26:31 +0300

    modify project0.py

---
+++ projects/project0.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import random
+import datetime
+
+
+def main():
+    dict1 = {"A": random.randint(0, 50), "B": random.randint(0, 50), "C": random.randint(0, 50)}
+    dict2 = {}
+    x = datetime.datetime.now()
+
+    path = 'stock1'
+    try:
+        os.makedirs(path)  # creating stock folder
+    except OSError as er:
+        print(er)
+    file1 = os.path.join(path, str(x) + '.txt')  # creating timestamp file
+    with open(file1, 'w') as f:
+        for key, value in dict1.items():
+            f.write('%s %s\n' % (key, value))
+
+    path = 'product1'
+    try:
+        os.makedirs(path)  # creating product folder
+    except OSError as er:
+        print(er)
+    file2 = os.path.join(path, str(x) + '.txt')  # creating timestamp file
+    with open(file1, 'r') as fr:
+        data = fr.readlines()
+        with open(file2, 'w') as fw:
+            for line in data:
+#                values = line.split()
+#                dict2['M'] = values[1]
+#                dict2['N'] = values[2]
+#                for key, value in dict2.items():
+#                    fw.write('%s %s\n' % (key, value))
+
+    return os.EX_OK
+
+
+if __name__ == "__main__":
+    sys.exit(main())

bf1e8495fd44479b157d3ee0edb6bba85bcbe6fd
vick-hub <amondev00@gmail.com>
2021-07-26 23:29:13 +0300

    modify project0.py

--- projects/project0.py
+++ projects/project0.py
@@ -3,37 +3,39 @@ import sys
 import random
 import datetime
 
+x = datetime.datetime.now()
+dict1 = {"A": random.randint(0, 50), "B": random.randint(0, 50), "C": random.randint(0, 50)}
+path = 'stock2'
+try:
+    os.makedirs(path)  # creating stock folder
+except OSError as er:
+    print(er)
+file1 = os.path.join(path, str(x) + '.txt')  # creating timestamp file
+with open(file1, 'w') as f:
+    for key, value in dict1.items():
+        print(key, value, file=f)
+
+path = 'product1'
+try:
+    os.makedirs(path)  # creating product folder
+except OSError as er:
+    print(er)
+file2 = os.path.join(path, str(x) + '.txt')  # creating timestamp file
+
+list1 = []
+with open(file1, 'r') as f:
+    def consumption(x, y, z):
+        lines = f.readlines()
+        for line in lines:
+            list1.append(line.split())
+        a = (list1[0][1])
+        b = list1[1][1]
+        c = list1[2][1]
+        return a, b, c
+
 
 def main():
-    dict1 = {"A": random.randint(0, 50), "B": random.randint(0, 50), "C": random.randint(0, 50)}
-    dict2 = {}
-    x = datetime.datetime.now()
-
-    path = 'stock'
-    try:
-        os.makedirs(path)  # creating stock folder
-    except OSError as er:
-        print(er)
-    file1 = os.path.join(path, str(x) + '.txt')  # creating timestamp file
-    with open(file1, 'w') as f:
-        for key, value in dict1.items():
-            f.write('%s %s\n' % (key, value))
-
-    path = 'product'
-    try:
-        os.makedirs(path)  # creating product folder
-    except OSError as er:
-        print(er)
-    file2 = os.path.join(path, str(x) + '.txt')  # creating timestamp file
-    with open(file1, 'r') as fr:
-        data = fr.readlines()
-        with open(file2, 'w') as fw:
-            for line in data:
-                values = line.split()
-#                dict2['M'] = values[1]
-#                dict2['N'] = values[2]
-#                for key, value in dict2.items():
-#                    fw.write('%s %s\n' % (key, value))
+    consumption(2, 3, 4)
 
     return os.EX_OK
 

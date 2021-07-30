229efeba7a9324dec2cab5bdf492aaf9ad63a234
vick-hub <amondev00@gmail.com>
2021-07-30 12:29:40 +0300

    modify project0.py

--- projects/project0.py
+++ projects/project0.py
@@ -2,89 +2,50 @@ import os
 import sys
 import random
 import datetime
+import glob
 
 
 x = datetime.datetime.now()
 dict1 = {"A": random.randint(0, 50), "B": random.randint(0, 50), "C": random.randint(0, 50)}
-path = 'stock'
+path = 'stock3'
 try:
     os.makedirs(path)  # creating stock folder
 except OSError as er:
     print(er)
 file1 = os.path.join(path, str(x) + '.txt')  # creating timestamp file
+list1 = []
+set1 = set()
+dict2 = {}
+dict3 = {}
 
 
 def stock():
     with open(file1, 'w') as f:
         for key, value in dict1.items():
-            print(key, value, file=f)
-
-
-list1 = []
-list2 = []
-dict2 = {}
-
-path1 = 'product'
-try:
-    os.makedirs(path1)  # creating product folder
-except OSError as er:
-    print(er)
-file2 = os.path.join(path1, str(x) + '.txt')  # creating timestamp file
-
-
-def product():
-    with open(file1, 'r') as fr:
-        with open(file2, 'w') as fw:
-            lines = fr.readlines()
-            for line in lines:
-                list1.append(line.split())
-            x = int(list1[0][1]) // 2
-            y = int(list1[1][1]) // 3
-            z = int(list1[2][1]) // 4
-            m = x + y + z
-            n = int(list1[0][1]) + z
-            dict2['M'] = m
-            dict2['N'] = n
-            for k, v in dict2.items():
-                print(k, v, file=fw)
+            f.write('%s %d\n' % (key, value))
 
 
 def stock_level():
-    with open(file1, 'r') as fr:
-        lines = fr.readlines()
-        for line in lines:
-            list1.append(line.split())
-        x = int(list1[0][1])
-        x += x
-        y = int(list1[1][1])
-        y += y
-        z = int(list1[2][1])
-        z += z
-        print(f"A {x}")
-        print(f"B {y}")
-        print(f"C {z}")
-
-
-def product_level():
-    with open(file2, 'r') as fw:
-        lines = fw.readlines()
-        for line in lines:
-            list2.append(line.split())
-        x = int(list2[0][1])
-        x += x
-        y = int(list2[1][1])
-        y += y
-        print(f"M {x}")
-        print(f"N {y}")
+    files = glob.glob('stock3/*.txt')  # help us to analyze multiple files
+#    for dirName, fileList in os.walk('stock2'):
+    for file in files:
+        with open(file, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                list1.append(line.split())
+        dict2.update(list1)
+        for key, value in dict2.items():
+            if key in dict3:
+                dict3[key] += int(value)
+            else:
+                dict3[key] = int(value)
+    for key, value in dict3.items():
+        print(key, value)
 
 
 def main():
     stock()
-    product()
-    print('stock level: ')
     stock_level()
-    print('product level: ')
-    product_level()
     return os.EX_OK
 
 
